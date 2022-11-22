9eb59dbb095bccbd20b1953f3de351b8d85baae4
root <root@SP22JCS1.sdc.bsnl.co.in>
2022-11-22 10:50:45 +0530

    docker deploy

--- InitialSetup/get_base_images_docker.sh
+++ InitialSetup/get_base_images_docker.sh
@@ -2,6 +2,6 @@ docker pull python:3.10-slim-bullseye
 docker pull debian:bullseye-slim
 docker pull kamailio/kamailio:5.4.6-buster
 
-docker build  -t agiserver:slim-python3.10 --build-arg https_proxy=$http_proxy --build-arg  https_proxy=$http_proxy  .
+docker build  -t agiserver:slim-python3.10 --build-arg https_proxy=$http_proxy --build-arg  https_proxy=$http_proxy -f ./Python3/Dockerfile
 
-docker build  -t asterisk:18-slim --build-arg https_proxy=$http_proxy --build-arg  https_proxy=$http_proxy .
+docker build  -t asterisk:18-slim --build-arg https_proxy=$http_proxy --build-arg  https_proxy=$http_proxy -f ./Asterisk/Dockerfile
